5f1996b0979c3bd89a3a4c1289c86532d3e64367
Ribich1 <ribich@ukr.net>
2023-07-24 17:46:20 +0200

    Search form

---
+++ src/components/Searchbar/Searchbar.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+export default class Searchbar extends Component {
+  state = {
+    imgFind: '',
+  };
+
+  
+  render() {
+    return (
+      <header className="searchbar">
+        <form className="form">
+          <button type="submit" class="button">
+            <span className="button-label">Search</span>
+          </button>
+
+          <input
+            className="input"
+            type="text"
+            autocomplete="off"
+            autofocus
+            placeholder="Search images and photos"
+          />
+        </form>
+      </header>
+    );
+  }
+}
